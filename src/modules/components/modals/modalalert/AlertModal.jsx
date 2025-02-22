5c84e35bbb1ba566bb15f0f1dadeca7852ad879e
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2025-02-21 22:12:24 -0300

    fix: modal can recive styles

--- src/modules/components/modals/modalalert/AlertModal.jsx
+++ src/modules/components/modals/modalalert/AlertModal.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Button } from '../../button/Button'
 
-export const AlertModal = ({ message, onClose, theme = 'light' }) => {
+export const AlertModal = ({
+	isOpen = false,
+	message,
+	onClose,
+	theme = 'light',
+	className = '',
+}) => {
+	if (!isOpen) return null
 	return (
 		<div className="modal-overlay">
-			<div className={`modal-content ${theme}`}>
+			<div className={`modal-content ${theme} ${className}`}>
 				<p>{message}</p>
 				<Button className="confirm-button" onEvent={onClose}>
 					Confirmar
